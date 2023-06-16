f88f2b24958aab2717a451c19075474ad53e18af
tr31zh <60819267+tr31zh@users.noreply.github.com>
2023-06-16 15:22:41 +0200

    Added Python support

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "pyright",
+        "texlab",
       })
     end,
   },
@@ -19,7 +21,11 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
-        -- "stylua",
+        "stylua",
+        "black",
+        "pylint",
+        "latexindent",
+        "vale",
       })
     end,
   },
@@ -29,7 +35,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
